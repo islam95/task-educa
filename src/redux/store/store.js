989e49196231903d0a5742d6ecb3ea136098a4a9
Islam Dudaev <islamwolf23@gmail.com>
2019-08-29 12:45:48 +0100

    Remove redux dev tools

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -6,8 +6,7 @@ import rootReducer from "./rootReducer";
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
   )
 );
 
